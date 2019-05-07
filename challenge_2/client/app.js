98040665ed215b07ce9989681ce239f21040045d
trevjnels <trevorjohnnels@gmail.com>
2019-05-07 15:38:56 -0700

    somehow forgot to commit so far. So much has been completed. I set up the express server, got the post route (mostly) working, though I'm not sure how to grab the CSV string, client side yet. Also serving up the html. All that is left is to set up client to read, send the file link, and create the nice 'single page web app' feel

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,117 @@
 
+// const https = require('https')
 
+
+//take in text input for JSON and do not disrupt the auto refresh while still getting the post request out there
+
+var newData = JSON.stringify({
+  "firstName": "Joshie",
+  "lastName": "Wyattson",
+  "county": "San Mateo",
+  "city": "San Mateo",
+  "role": "Broker",
+  "sales": 1000000,
+  "children": [
+  {
+    "firstName": "Beth Jr.",
+    "lastName": "Johnson",
+    "county": "San Mateo",
+    "city": "Pacifica",
+    "role": "Manager",
+    "sales": 2900000,
+    "children": [
+      {
+        "firstName": "Smitty",
+        "lastName": "Won",
+        "county": "San Mateo",
+        "city": "Redwood City",
+        "role": "Sales Person",
+        "sales": 4800000,
+        "children": []
+      },
+      {
+        "firstName": "Allen",
+        "lastName": "Price",
+        "county": "San Mateo",
+        "city": "Burlingame",
+        "role": "Sales Person",
+        "sales": 2500000,
+        "children": []
+      }
+    ]
+  },
+  {
+    "firstName": "Beth",
+    "lastName": "Johnson",
+    "county": "San Francisco",
+    "city": "San Francisco",
+    "role": "Broker/Sales Person",
+    "sales": 7500000,
+    "children": []
+  }
+]
+})
+
+var options = {
+  post: {
+    hostname: '',
+    port: 3000,
+    path: '/',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+}
+var formListner = () => {
+
+  console.log("Yo" , newData)
+    var form = Array.from(document.getElementsByClassName('jToC'))
+    var elem =  form[0]
+
+    elem.onclick = function(){
+      // var jsonD = JSON.stringify(sampleJSON)
+      fetch('/', {
+      
+          method: 'POST',
+          body: newData,
+          headers: {"Content-Type": "application/json"},
+        }).then(res => {
+          console.log(res)
+        }).catch(anythingElse => {
+          console.log("what is"  , anythingElse)
+        })
+    }
+}
+formListner()
+// form[0].onclick(function(){
+// fetch('/', {
+//   headers: {"Content-Type": "application/json; charset=utf8"},
+//   method: 'POST',
+//   body: JSON.stringify(sampleJSON)
+// }).then(res => {
+//   console.log(res)
+// }).catch(anythingElse => {
+//   console.log("what is"  , anythingElse)
+// })
+// })
+
+
+
+
+
+
+
+//will receive CSV formatted result 
+
+
+
+
+//page relodes upon receipt with csv file and input feild, ready to go. No refresh tho.
+
+
+
+
+
+//eventually add a link to download the csv file
+//can use jquery to manipulate dom now and ajax etc
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,132 @@
+const express = require('express')
+const app = express();
+const bodyParser  = require('body-parser')
+const fs = require('fs')
 
+const port = 3000
+const path = require('path')
+var num = 0;
+
+
+app.use(express.static('client'));
+app.use(bodyParser.urlencoded({extended: true }))
+app.use(bodyParser.json())
+app.get('/', (req, res) => res.status(200).sendFile("/client/index.html"))
+
+app.post('/', (req, res) => {
+
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  console.log("request is - - - - -- - -- - -- - - - ", req.body)
+      var jsonObj = req.body
+      var CSVstringPath = jsonToCSV(jsonObj)
+
+      fs.readFile(path.join(__dirname + CSVstringPath), {encoding: 'utf8'}, (err, data)=>{
+        if(err){
+          console.log("THERE WAS AN ERROR serverSide", err)
+        } else {
+          console.log(data, " * * * *  ** * * *  ** * * *  ** * * *  ** * * *  *data")
+        }
+        res.status(200).send(`${data}`)
+
+      })
+      
+      // console.log("post request received")
+    }
+)
+app.listen(port, () => {
+  console.log('server is lightly joggin on port: ', port)
+})
+
+
+
+//receive post requests and return data like the csv below
+
+//post may be in URL weirdly?
+
+var sample= {
+    "firstName": "Joshie",
+    "lastName": "Wyattson",
+    "county": "San Mateo",
+    "city": "San Mateo",
+    "role": "Broker",
+    "sales": 1000000,
+    "children": [
+    {
+      "firstName": "Beth Jr.",
+      "lastName": "Johnson",
+      "county": "San Mateo",
+      "city": "Pacifica",
+      "role": "Manager",
+      "sales": 2900000,
+      "children": [
+        {
+          "firstName": "Smitty",
+          "lastName": "Won",
+          "county": "San Mateo",
+          "city": "Redwood City",
+          "role": "Sales Person",
+          "sales": 4800000,
+          "children": []
+        },
+        {
+          "firstName": "Allen",
+          "lastName": "Price",
+          "county": "San Mateo",
+          "city": "Burlingame",
+          "role": "Sales Person",
+          "sales": 2500000,
+          "children": []
+        }
+      ]
+    },
+    {
+      "firstName": "Beth",
+      "lastName": "Johnson",
+      "county": "San Francisco",
+      "city": "San Francisco",
+      "role": "Broker/Sales Person",
+      "sales": 7500000,
+      "children": []
+    }
+  ]
+};
+
+
+var jsonToCSV = function(data) {
+  num++
+  var keys = Object.keys(data);
+  keys.pop()
+  var joinedK = keys.join(",")
+  fs.writeFileSync(`./file${num}`, joinedK)
+
+  var recurser = function(obj) {
+    var values = keys.map(key => {
+      return obj[key];
+    })
+   //removes children before string parsing
+    var joinedV = "\n" + values.join(",")
+    // console.log("keys are ", joinedV)
+    fs.appendFileSync(`./file${num}`, joinedV)
+    if(obj.children.length){
+      obj.children.map(child => {
+        recurser(child)
+      })
+    }
+  }
+  recurser(data)
+ return `/file${num}`;
+}
+
+
+var sampleCSV = 
+`firstName,lastName,county,city,role,sales
+Joshie,Wyattson,San Mateo,San Mateo,Broker,1000000
+Beth Jr.,Johnson,San Mateo,Pacifica,Manager,2900000
+Smitty,Won,San Mateo,Redwood City,Sales Person,4800000
+Allen,Price,San Mateo,Burlingame,Sales Person,2500000
+Beth,Johnson,San Francisco,San Francisco,Broker/Sales Person,7500000`;
+
+
+module.exports = app;
 
