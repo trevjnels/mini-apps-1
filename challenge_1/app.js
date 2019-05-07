7e88bfcaa2f9a8b131da569e4af26934b8212710
trevjnels <trevorjohnnels@gmail.com>
2019-05-06 18:22:16 -0700

    core functionality is all there. You can get the 'full' expierence by clicking through and refershing the page. Need to put a reset button listener on, switch the starting player, and refactor to use a slightly smarter approach prehaps?

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,44 +1,134 @@
+
+
+
 var board = {
-  row1: ["o",0,'x'],
-  row2: [1,'x',1],
-  row3: ['o','o','o'],
-  lastWinner: null,
-  score: {
-    player1: 0,
-    payer2: 0
+
+     board: [
+          ["","",""],
+          ["","",""],
+          ['','',' '],
+      ],
+
+  currentPlayer: "player1",
+  players: {
+    key: {
+      player1: "x",
+      player2: "o"
+    },
+    src: {
+      "player1": "https://media3.giphy.com/media/xT9Igpm06uM5OJ5lVS/source.gif",
+      "player2": "https://media.giphy.com/media/l3q2NvqacEdhTJ6Mw/giphy.gif"
+    },
+    score: {
+      player1: 0,
+      payer2: 0
+    },
+}
+}
+
+
+
+
+
+var rowLooper = [board.board.row1, board.board.row2, board.board.row3,]
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - -
+// - - - - - - - - - - - - - - - - - - - - - - - - - -
+// - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+
+
+
+var playerToggle = function(obj){
+if(obj.currentPlayer === "player1") {
+  obj.currentPlayer = "player2"
+} else {
+  obj.currentPlayer = "player1"
+}
+}
+
+var headerRender = function(input) {
+
+  if(input) {
+if(Object.keys(input)[0] ===  "win"){
+  var header = document.getElementsByClassName("title")
+  header[0].innerHTML = `<h1 style="color: red">Player ${input.win.toUpperCase()} WINS</h1>`
+}
   }
-}
-var boardMap = {
-    a:  board.row1[0] ,
-    b:   board.row1[1] ,
-    c:   board.row1[2] ,
-    d:    board.row2[0] ,
-    e:  board.row2[1] ,
-    f:   board.row2[2],
-    g:    board.row3[0],
-    h:   board.row3[1],
-    i:   board.row3[2]
-}
 
-var rowLooper = [board.row1, board.row2, board.row3,]
-
-var gameLogic = function(board) {
-  //switching between players - start with X
+// changes header from f
+//Tic tac toe (title)
+//to firstPlayer - you go first
+//back to tic tac toe
+// to winningPlayer "WINS!!"
 
 }
+var scoreRender = function(){
+  //optional
+}
+
+var squareListner = function(board){
+  var squareNodes = Array.from(document.getElementsByClassName("square"));
+  squareNodes.map(elem => {
+
+    elem.onclick = function(e) {
+      var resultsBoard = board.board.slice()
+      var clicked = elem.id
+      
+      var {currentPlayer, players} = board;
+      var playerLetter = players.key[currentPlayer];
+      console.log("player letter is ", typeof playerLetter)
+      console.log("****** spaceKey = ", clicked)
+      var split = clicked.split("")
+    
+      resultsBoard[split[0]][split[1]] = playerLetter;
+     
+      console.log(JSON.stringify( resultsBoard));
+      board.board = resultsBoard;
+      elem.onclick = null;
+
+      elem.innerHTML = `<img class="selected-${playerLetter}" src = ${players.src[currentPlayer]} / >`
+      if(boardChecker(board.board, playerLetter)){
+        headerRender({win: playerLetter})
+        players.score[currentPlayer] ++ 
+      }
+      playerToggle(board);
+    }
+  })
+  }
+
+
+
+
+
+var buttonsListner = function(){
+
+
+}
+
+
+
+
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - -
+// - - - - - - - - - - - - - - - - - - - - - - - - - -
+// - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+
+
 
 var boardChecker = function(board, player){
   if(diagChecker(board, player)){
-    alert(`${player} wins! diag`)
+    // alert(`${player} wins! diag`)
     return true;
   }
   var num =0
   while(num <3) {
-   var row = rowChecker(rowLooper[num], player)
-   var cols = colChecker(num, player)
+   var row = rowChecker(board[num], player)
+   var cols = colChecker(board, num, player)
     num++
     if(row || cols){
-      alert(`${player} wins! col/row`)
+      // alert(`${player} wins! col/row`)
       return true;
     }
   }
@@ -59,9 +149,9 @@ var rowChecker = function(row, player){
     return false;
   }
 }
-var colChecker = function(col, player){
+var colChecker = function(board, col, player){
   var totalX = 0
-  rowLooper.forEach(row => {
+  board.forEach(row => {
     var box = row[col]
     if(box === player){
       totalX++
@@ -76,16 +166,23 @@ var colChecker = function(col, player){
 }
 var diagChecker = function(board, player){
   playerThree = player+player+player
-  var {row1, row2, row3} = board;
-  if((row1[0] + row2[1] + row3[2]) === playerThree) {
+  if((board[0][0] + board[1][1] + board[2][2]) === playerThree) {
     console.log("majorDiagnal present")
     return true
   }
-  if((row1[2] + row2[1] + row3[0])=== playerThree) {
+  if((board[2][0] + board[1][1]  + board[0][2])=== playerThree) {
     console.log("minordiag present")
     return true
   }
   return false
 }
 
-console.log(boardChecker(board, 'o'))
+// console.log(boardChecker(board.board, 'o'))
+
+
+var starter = function () {
+console.log('starter is starting')
+squareListner(board)
+
+}
+starter()
